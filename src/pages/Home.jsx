6b5b910e2be850f1e8657c0835a62c9ce160a363
abniyou-dev <abniyouismail404@gmail.com>
2025-12-27 06:40:17 -0800

    setting up the workspace

---
+++ src/pages/Home.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+function Home() {
+
+  // download the json-server package 
+  // npm install -g json-server
+
+
+  // run the json server 
+  // json-server --watch db.json --port 5000
+
+
+  return (
+    <div
+      className='w-full min-h-screen bg-neutral-900 text-white'>
+        Hi  THere 🙋‍♂️
+    </div>
+  )
+}
+
+export default Home;
