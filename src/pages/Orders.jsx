bd6cb0d5c9450eb0889ac46cf0a866b6bb77dfbd
abniyou-dev <abniyouismail404@gmail.com>
2025-12-29 14:43:33 -0800

    fixing the table in the orders page

--- src/pages/Orders.jsx
+++ src/pages/Orders.jsx
@@ -4,12 +4,13 @@ import { BiSearch } from "react-icons/bi";
 import { ordersData } from "../data/OrdersData";
 
 function orders() {
-
   const [serchedWord, setSearchedWord] = useState();
   const [filtredData, setFilteresData] = useState([]);
 
   function handleChange(e) {
-    setFilteresData(ordersData.filter(or => or.customer.includes(e.target.value)));
+    setFilteresData(
+      ordersData.filter((or) => or.customer.includes(e.target.value))
+    );
   }
 
   function handleclick() {
@@ -22,11 +23,12 @@ function orders() {
         <SideBar />
       </div>
 
-      <div className="p-5 space-y-5 w-full">
+      <div className="p-5 space-y-5 w-full overflow-x-hidden">
         <div className="grid">
           <h1 className="text-4xl font-bold tracking-wider">Orders</h1>
-          <span className="text-sm text-neutral-400">Total Orders : {ordersData.length}</span>
-
+          <span className="text-sm text-neutral-400">
+            Total Orders : {ordersData.length}
+          </span>
         </div>
 
         {/* searchh box */}
@@ -43,27 +45,27 @@ function orders() {
         </div>
 
         {/* data table */}
-        <div className="min-w-154 ">
-          <div className="w-full border border-neutral-700">
-            <div
-              className="grid grid-cols-5  text-center border-neutral-700
-              [&>span]:p-3 [&>span]:border-r [&>span]:border-neutral-700 
-              [&>span]:font-bold [&>span]:text-neutral-200"
-            >
-              <span>id</span>
-              <span>customer</span>
-              <span>total</span>
-              <span>status</span>
-              <span>date</span>
-            </div>
+        {/* data table */}
+        <div className="relative w-full">
+          <div className="overflow-x-auto">
+            <div className="min-w-155 border border-neutral-700">
+              <div
+                className="grid grid-cols-5 text-center border-neutral-700
+                          [&>span]:p-3 [&>span]:border-r [&>span]:border-neutral-700 
+                          [&>span]:font-bold [&>span]:text-neutral-200">
+                <span>id</span>
+                <span>customer</span>
+                <span>total</span>
+                <span>status</span>
+                <span>date</span>
+              </div>
 
-            {filtredData.length > 0 ? (
-              filtredData.map((or) => (
+              {(filtredData.length > 0 ? filtredData : ordersData).map((or) => (
                 <div
                   key={or.id}
                   className="grid grid-cols-5 border-t text-center border-neutral-700
-              [&>span]:p-3 [&>span]:border-r [&>span]:border-neutral-700 
-             [&>span]:text-neutral-200"
+          [&>span]:p-3 [&>span]:border-r [&>span]:border-neutral-700 
+          [&>span]:text-neutral-200"
                 >
                   <span>{or.id}</span>
                   <span>{or.customer}</span>
@@ -71,29 +73,20 @@ function orders() {
                   <span>{or.status}</span>
                   <span>
                     <span
-                    className={or.status === "pending" ? "pendding" : or.status === "shipped" ?  "shipped" : "delivered"}>{or.status}</span>
+                      className={
+                        or.status === "pending"
+                          ? "pendding"
+                          : or.status === "shipped"
+                          ? "shipped"
+                          : "delivered"
+                      }
+                    >
+                      {or.status}
                     </span>
+                  </span>
                 </div>
-              ))
-            ) : (
-              ordersData.map((or) => (
-                <div
-                  key={or.id}
-                  className="grid grid-cols-5 border-t text-center border-neutral-700
-              [&>span]:p-3 [&>span]:border-r [&>span]:border-neutral-700 
-             [&>span]:text-neutral-200"
-                >
-                  <span>{or.id}</span>
-                  <span>{or.customer}</span>
-                  <span>{or.total}</span>
-                  <span>{or.status}</span>
-                  <span>
-                    <span
-                    className={or.status === "pending" ? "pendding" : or.status === "shipped" ?  "shipped" : "delivered"}>{or.status}</span>
-                    </span>
-                </div>
-              ))
-            )}
+              ))}
+            </div>
           </div>
         </div>
       </div>
